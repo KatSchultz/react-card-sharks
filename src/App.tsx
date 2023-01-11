837d2ff7609526ff8ad37409064d33cbdfe9533d
KatSchultz <ks.katieschultz@gmail.com>
2023-01-11 15:20:42 -0500

    added card images, board and card components

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header/Header";
 import Directions from "./components/Directions/Directions";
+import GameBoard from "./components/GameBoard/GameBoard";
 
 function App() {
+  const cards = [
+    "img-0.png",
+    "img-1.png",
+    "img-3.png",
+    "img-4.png",
+    "img-5.png",
+    "img-6.png",
+    "img-7.png",
+    "img-8.png",
+    "img-9.png",
+    "img-10.png",
+    "img-11.png",
+    "img-12.png",
+  ];
   return (
     <div className="App">
       <Header />
       <Directions />
+      <GameBoard />
     </div>
   );
 }
