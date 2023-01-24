d5848c0a6dfa3d0720e6ccc813d891cdd0df72cb
KatSchultz <ks.katieschultz@gmail.com>
2023-01-24 14:50:55 -0500

    trying grid2 in GameBoard

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -3,6 +3,7 @@ import "./GameBoard.css";
 import Card from "../Cards/PlayingCard";
 import { PlayingCard } from "../../types";
 import CardDirectImport from "../Cards/PlayingCardDirectImport";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 interface Props {
   cards: PlayingCard[];
@@ -29,22 +30,42 @@ export default function GameBoard({
   gameOver,
 }: Props) {
   return (
-    <div className="game-board">
-      {cards.map((card) => (
-        <CardDirectImport
-          key={card.id}
-          card={card}
-          flippedCards={flippedCards}
-          trackFlips={trackFlips}
-          noMatchFlip={noMatchFlip}
-          foundPairs={foundPairs}
-          flipCount={flipCount}
-          setFlipCount={setFlipCount}
-          timerActive={timerActive}
-          gameCount={gameCount}
-          gameOver={gameOver}
-        />
-      ))}
+    // <div className="game-board">
+    //   {cards.map((card) => (
+    //     <CardDirectImport
+    //       key={card.id}
+    //       card={card}
+    //       flippedCards={flippedCards}
+    //       trackFlips={trackFlips}
+    //       noMatchFlip={noMatchFlip}
+    //       foundPairs={foundPairs}
+    //       flipCount={flipCount}
+    //       setFlipCount={setFlipCount}
+    //       timerActive={timerActive}
+    //       gameCount={gameCount}
+    //       gameOver={gameOver}
+    //     />
+    //   ))}
+    // </div>
+    <div style={{ height: "fit-content" }}>
+      <Grid2 container spacing={1}>
+        {cards.map((card) => (
+          <Grid2 xs={4} sm={4} md={3} key={card.id}>
+            <Card
+              card={card}
+              flippedCards={flippedCards}
+              trackFlips={trackFlips}
+              noMatchFlip={noMatchFlip}
+              foundPairs={foundPairs}
+              flipCount={flipCount}
+              setFlipCount={setFlipCount}
+              timerActive={timerActive}
+              gameCount={gameCount}
+              gameOver={gameOver}
+            />
+          </Grid2>
+        ))}
+      </Grid2>
     </div>
   );
 }
